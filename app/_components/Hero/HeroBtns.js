c9b3ae2542d7c7a9d28d2b4652e72e4981212eb1
MohammedGhaly <medooghaly7@gmail.com>
2025-08-16 07:34:31 +0300

    link to the updated cv added

--- app/_components/Hero/HeroBtns.js
+++ app/_components/Hero/HeroBtns.js
@@ -15,7 +15,7 @@ function HeroBtns() {
         className="text-darkBlue bg-white hover:brightness-90"
         onClick={() =>
           window.open(
-            "https://drive.google.com/file/d/1F3Lb0I-QZ7lmD0i8teyP8lYnd3hSiHRQ/view?usp=sharing",
+            "https://drive.google.com/file/d/1CXqCieJ50sZMMMQBkHh0Dx0pIcR-l3u6/view?usp=sharing",
             "_blank",
           )
         }
